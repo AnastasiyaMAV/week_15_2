7841571bb471d1acbf5dbfb490eac59927b82d1b
AnastasiyaMAV <mihalukova112@mail.ru>
2021-09-14 16:33:30 +0200

    Задание выполнено

---
+++ index.js
@@ -0,0 +1,18 @@
+function cheakInput() {
+    let nickName = document.getElementById('formGroupNickname').value;
+    let email = document.getElementById('exampleInputEmail').value;
+    let password = document.getElementById('exampleInputPassword').value;
+    let tel = document.getElementById('exampleInputTel').value;
+
+    if(nickName == ""){
+        alert('Ввидите, пожалуйста, Никнейм!');
+    } else if(email == ""){
+        alert('Введите, пожалуйста, Email!');
+    } else if(password == ""){
+        alert('Введите, пожалуйтса, пароль!');
+    } else if(tel == ""){
+        alert('Введите, пожалуйста, телефон!');
+    } else {
+        alert('Добро пожаловать, ' + nickName + '!');
+    }
+}
